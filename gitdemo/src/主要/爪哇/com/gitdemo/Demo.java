11a3a33e9e9f56d196fe2d8f78ff6d58f97dcf1d
yuanbo3477 <yuanbo3477@163.com>
2020-12-20 16:23:22 +0800

    aaa

--- gitdemo/src/主要/爪哇/com/gitdemo/Demo.java
+++ gitdemo/src/主要/爪哇/com/gitdemo/Demo.java
@@ -3,7 +3,11 @@ package com.gitdemo;
 public class Demo {
 
     public static void main(String[] args) {
+<<<<<<< Updated upstream:gitdemo/src/主要/爪哇/com/gitdemo/Demo.java
 1111
+=======
+        System.out.println("111");
+>>>>>>> Stashed changes:gitdemo/src/main/java/com/gitdemo/Demo.java
     }
 
 }
